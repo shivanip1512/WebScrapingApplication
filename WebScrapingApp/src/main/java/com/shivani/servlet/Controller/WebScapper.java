486ce521dcb9122ca43f36da1e34130f96db1611
shivanip1512 <shivanipacharne10@gmail.com>
2021-09-12 23:25:09 +0530

    added index page

--- WebScrapingApp/src/main/java/com/shivani/servlet/Controller/WebScapper.java
+++ WebScrapingApp/src/main/java/com/shivani/servlet/Controller/WebScapper.java
@@ -1,6 +1,8 @@
 package com.shivani.servlet.Controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,9 +19,12 @@ public class WebScapper extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String url = "https://www.imdb.com/india/top-rated-indian-movies/";//request.getParameter("url");
+		String url = request.getParameter("url");//"https://www.imdb.com/india/top-rated-indian-movies/";
 		
 		Elements trElements = ImdbScraperModel.processData(url);
+		
+		RequestDispatcher rd = request.getRequestDispatcher("View.jsp");
+		rd.forward(request, response);
 		
 		for (Element tr : trElements) {
 			String movieTitle = tr.select("td.titleColumn > a").text();
---
+++ WebScrapingApp/src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	<a href="webScapper?url=https://www.imdb.com/india/top-rated-indian-movies/">Top
+		Bollywood movies</a>
+</body>
+</html>
